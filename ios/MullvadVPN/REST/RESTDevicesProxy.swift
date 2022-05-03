c72de1f1d60cceb8b35dda5d8273eb67cd205d88
Andrej Mihajlov <and@mullvad.net>
2022-05-03 12:02:59 +0200

    REST: add new methods in device proxy

--- ios/MullvadVPN/REST/RESTDevicesProxy.swift
+++ ios/MullvadVPN/REST/RESTDevicesProxy.swift
@@ -24,6 +24,76 @@ extension REST {
             )
         }
 
+        /// Fetch device by identifier.
+        /// The completion handler receives `nil` if device is not found.
+        func getDevice(
+            accountNumber: String,
+            identifier: String,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping CompletionHandler<Device?>
+        ) -> Cancellable
+        {
+            let requestHandler = AnyRequestHandler(
+                createURLRequest: { endpoint, authorization in
+                    let urlEncodedIdentifier = identifier
+                        .addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+                    let path = "device/\(urlEncodedIdentifier)"
+
+                    var requestBuilder = self.requestFactory.createURLRequestBuilder(
+                        endpoint: endpoint,
+                        method: .get,
+                        path: path
+                    )
+
+                    requestBuilder.setAuthorization(authorization)
+
+                    return requestBuilder.getURLRequest()
+                },
+                requestAuthorization: { completion in
+                    return self.configuration.accessTokenManager
+                        .getAccessToken(
+                            accountNumber: accountNumber,
+                            retryStrategy: retryStrategy
+                        ) { operationCompletion in
+                            completion(operationCompletion.map { tokenData in
+                                return .accessToken(tokenData.accessToken)
+                            })
+                        }
+                }
+            )
+
+            let responseHandler = AnyResponseHandler { response, data -> ResponseHandlerResult<Device?> in
+                let httpStatus = HTTPStatus(rawValue: response.statusCode)
+
+                switch httpStatus {
+                case let httpStatus where httpStatus.isSuccess:
+                    return .decoding {
+                        return try self.responseDecoder.decode(Device.self, from: data)
+                    }
+
+                case .notFound:
+                    return .success(nil)
+
+                default:
+                    return .unhandledResponse(
+                        try? self.responseDecoder.decode(
+                            ServerErrorResponse.self,
+                            from: data
+                        )
+                    )
+                }
+            }
+
+            return addOperation(
+                name: "get-device",
+                retryStrategy: retryStrategy,
+                requestHandler: requestHandler,
+                responseHandler: responseHandler,
+                completionHandler: completion
+            )
+        }
+
+        /// Fetch a list of created devices.
         func getDevices(
             accountNumber: String,
             retryStrategy: REST.RetryStrategy,
@@ -69,6 +139,196 @@ extension REST {
             )
         }
 
+        /// Create new device.
+        /// The completion handler will receive a `CreateDeviceResponse.created(Device)` on success.
+        /// Other `CreateDeviceResponse` variants describe errors.
+        func createDevice(
+            accountNumber: String,
+            request: CreateDeviceRequest,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping CompletionHandler<Device>
+        ) -> Cancellable
+        {
+            let requestHandler = AnyRequestHandler(
+                createURLRequest: { endpoint, authorization in
+                    var requestBuilder = self.requestFactory.createURLRequestBuilder(
+                        endpoint: endpoint,
+                        method: .post,
+                        path: "devices"
+                    )
+                    requestBuilder.setAuthorization(authorization)
+
+                    try requestBuilder.setHTTPBody(value: request)
+
+                    return requestBuilder.getURLRequest()
+                },
+                requestAuthorization: { completion in
+                    return self.configuration.accessTokenManager
+                        .getAccessToken(
+                            accountNumber: accountNumber,
+                            retryStrategy: retryStrategy
+                        ) { operationCompletion in
+                            completion(operationCompletion.map { tokenData in
+                                return .accessToken(tokenData.accessToken)
+                            })
+                        }
+                }
+            )
+
+            let responseHandler = REST.defaultResponseHandler(
+                decoding: Device.self,
+                with: responseDecoder
+            )
+
+            return addOperation(
+                name: "create-device",
+                retryStrategy: retryStrategy,
+                requestHandler: requestHandler,
+                responseHandler: responseHandler,
+                completionHandler: completion
+            )
+        }
+
+        /// Delete device by identifier.
+        /// The completion handler will receive `true` if device is successfully removed,
+        /// otherwise `false` if device is not found or already removed.
+        func deleteDivice(
+            accountNumber: String,
+            identifier: String,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping CompletionHandler<Bool>
+        ) -> Cancellable
+        {
+            let requestHandler = AnyRequestHandler(
+                createURLRequest: { endpoint, authorization in
+                    let urlEncodedIdentifier = identifier
+                        .addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+                    let path = "devices/".appending(urlEncodedIdentifier)
+
+                    var requestBuilder = self.requestFactory
+                        .createURLRequestBuilder(
+                            endpoint: endpoint,
+                            method: .delete,
+                            path: path
+                        )
+
+                    requestBuilder.setAuthorization(authorization)
+
+                    return requestBuilder.getURLRequest()
+                },
+                requestAuthorization: { completion in
+                    return self.configuration.accessTokenManager
+                        .getAccessToken(
+                            accountNumber: accountNumber,
+                            retryStrategy: retryStrategy
+                        ) { operationCompletion in
+                            completion(operationCompletion.map { tokenData in
+                                return .accessToken(tokenData.accessToken)
+                            })
+                        }
+                }
+            )
+
+            let responseHandler = AnyResponseHandler { response, data -> ResponseHandlerResult<Bool> in
+                let statusCode = HTTPStatus(rawValue: response.statusCode)
+
+                switch statusCode {
+                case let statusCode where statusCode.isSuccess:
+                    return .success(true)
+
+                case .notFound:
+                    return .success(false)
+
+                default:
+                    return .unhandledResponse(
+                        try? self.responseDecoder.decode(
+                            ServerErrorResponse.self,
+                            from: data
+                        )
+                    )
+                }
+            }
+
+            return addOperation(
+                name: "delete-device",
+                retryStrategy: retryStrategy,
+                requestHandler: requestHandler,
+                responseHandler: responseHandler,
+                completionHandler: completion
+            )
+        }
+
+        /// Rotate device key
+        func rotateDeviceKey(
+            accountNumber: String,
+            identifier: String,
+            publicKey: PublicKey,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping CompletionHandler<Device>
+        ) -> Cancellable {
+            let requestHandler = AnyRequestHandler(
+                createURLRequest: { endpoint, authorization in
+                    let urlEncodedIdentifier = identifier
+                        .addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+                    let path = "devices/\(urlEncodedIdentifier)/pubkey"
+
+                    var requestBuilder = self.requestFactory
+                        .createURLRequestBuilder(
+                            endpoint: endpoint,
+                            method: .put,
+                            path: path
+                        )
+
+                    requestBuilder.setAuthorization(authorization)
+
+                    let request = RotateDeviceKeyRequest(
+                        pubkey: publicKey.base64Key
+                    )
+                    try requestBuilder.setHTTPBody(value: request)
+
+                    return requestBuilder.getURLRequest()
+                },
+                requestAuthorization: { completion in
+                    return self.configuration.accessTokenManager
+                        .getAccessToken(
+                            accountNumber: accountNumber,
+                            retryStrategy: retryStrategy
+                        ) { operationCompletion in
+                            completion(operationCompletion.map { tokenData in
+                                return .accessToken(tokenData.accessToken)
+                            })
+                        }
+                }
+            )
+
+            let responseHandler = REST.defaultResponseHandler(
+                decoding: Device.self,
+                with: responseDecoder
+            )
+
+            return addOperation(
+                name: "rotate-device-key",
+                retryStrategy: retryStrategy,
+                requestHandler: requestHandler,
+                responseHandler: responseHandler,
+                completionHandler: completion
+            )
+        }
+
+    }
+
+    struct CreateDeviceRequest: Encodable {
+        let pubkey: String
+        let hijackDNS: Bool
+
+        private enum CodingKeys: String, CodingKey {
+            case hijackDNS = "hijackDns"
+            case pubkey
+        }
+    }
+
+    fileprivate struct RotateDeviceKeyRequest: Encodable {
+        let pubkey: String
     }
 
     struct Device: Decodable {
